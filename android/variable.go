ccbf56a32f6960174a483cd1af8063f14fb67a06
mickybart <mickybart@pygoscelis.org>
2017-09-08 11:17:45 -0400

    variables: Add legacy_blob_compatible product variable

--- android/variable.go
+++ android/variable.go
@@ -110,6 +110,12 @@ type variableProperties struct {
 		Uses_qcom_bsp_legacy struct {
 			Cflags []string
 		}
+
+		Legacy_blob_compatible struct {
+			Cflags   []string
+			Cppflags []string
+			Srcs     []string
+		}
 	} `android:"arch_variant"`
 }
 
@@ -188,6 +194,7 @@ type productVariables struct {
 	Needs_text_relocations *bool `json:",omitempty"`
 	Target_shim_libs *string `json:",omitempty"`
 	Uses_qcom_bsp_legacy *bool `json:",omitempty"`
+	Legacy_blob_compatible *bool `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
